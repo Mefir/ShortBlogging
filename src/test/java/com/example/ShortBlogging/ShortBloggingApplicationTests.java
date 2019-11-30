01d89beff57bc0343615c7a76831a652112febd7
Bartosz <drrizd@gmail.com>
2019-11-30 12:38:29 +0100

    first commit

---
+++ src/test/java/com/example/ShortBlogging/ShortBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShortBlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
