fe0a2a5c6c490b40cce75acb2a00a885a680540c
Bartosz <drrizd@gmail.com>
2019-12-08 13:44:43 +0100

    Podstawa

--- src/main/java/com/example/ShortBlogging/ShortBloggingApplication.java
+++ src/main/java/com/example/ShortBlogging/ShortBloggingApplication.java
@@ -3,6 +3,7 @@ package com.example.ShortBlogging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShortBloggingApplication {
 
